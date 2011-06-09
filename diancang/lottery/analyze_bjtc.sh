f28611cf3a1a08e26708bccc40a4e28f735f8a5e
guiwuu <guiwuu@gmail.com>
2011-06-10 01:20:00 +0800

    add analysis by time and by issue

--- diancang/lottery/analyze_bjtc.sh
+++ diancang/lottery/analyze_bjtc.sh
@@ -1,19 +1,28 @@
 log_file=$1
+issue_id=$2
 
 if [ -z $log_file ];then
         echo "please specifc a bjtc log file"
         exit 0
 fi
 
-tb_order_num=`grep 出票调用 $log_file | wc -l`
+if [ -z $issue_id ];then
+        issue_id='WARN'
+fi
+
+tb_order_num=`grep 出票调用 $log_file | grep $issue_id | wc -l`
 echo "订单总数：$tb_order_num"
 
-ll_order_num=`grep 出票调用 $log_file | awk -F， '{print $3}' | sed "s/共//g" | sed "s/票//g" | awk '{a+=$1}END{print a}'`
+ll_order_num=`grep 出票调用 $log_file | grep $issue_id | awk -F， '{print $3}' | sed "s/共//g" | sed "s/票//g" | awk '{a+=$1}END{print a}'`
 echo "彩票总数：$ll_order_num"
 
-agent_total_ms=`grep 出票调用 $log_file | awk -F， '{print $4}' | sed "s/耗时 //g" | sed "s/ms//g" | awk '{a+=$1}END{print a}'`
-cost_per_tb_order=`expr $agent_total_ms / $tb_order_num`
-cost_per_ll_order=`expr $agent_total_ms / $ll_order_num`
+agent_total_ms=`grep 出票调用 $log_file |  grep $issue_id | awk -F， '{print $4}' | sed "s/耗时 //g" | sed "s/ms//g" | awk '{a+=$1}END{print a}'`
+total_ms=`grep executed $log_file | grep $issue_id | awk '{print $8}' | sed 's/ms//g' | awk '{a+=$1}END{print a}'`
 
+agent_cost_per_tb_order=`expr $agent_total_ms / $tb_order_num`
+agent_cost_per_ll_order=`expr $agent_total_ms / $ll_order_num`
+cost_per_tb_order=`expr $total_ms / $tb_order_num`
+
+echo "代理商订单平均耗时：${agent_cost_per_tb_order}ms"
+echo "代理商彩票平均耗时：${agent_cost_per_ll_order}ms"
 echo "订单平均耗时：${cost_per_tb_order}ms"
-echo "彩票平均耗时：${cost_per_ll_order}ms"
