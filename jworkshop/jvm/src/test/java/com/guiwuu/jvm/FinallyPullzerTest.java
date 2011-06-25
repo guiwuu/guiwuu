1691ee2c7aa25902ffd962774aa0efd3551fd3dc
guiwuu <guiwuu@gmail.com>
2011-06-25 19:29:09 +0800

    add jvm workshopt project

---
+++ jworkshop/jvm/src/test/java/com/guiwuu/jvm/FinallyPullzerTest.java
@@ -0,0 +1,16 @@
+package com.guiwuu.jvm;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author guiwuu
+ */
+public class FinallyPullzerTest {
+
+    @Test
+    public void test() {
+        assertEquals(1, FinallyPuzller.call());
+    }
+}
