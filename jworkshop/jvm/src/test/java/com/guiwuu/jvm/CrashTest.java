b8d64cd6980888235555cf301c682fafdd21967c
guiwuu <guiwuu@gmail.com>
2011-07-19 00:56:59 +0800

    add jvm crash test

---
+++ jworkshop/jvm/src/test/java/com/guiwuu/jvm/CrashTest.java
@@ -0,0 +1,15 @@
+package com.guiwuu.jvm;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author guiwuu
+ */
+public class CrashTest {
+    
+    @Test
+    public void testCrash() {
+        Crash.crash();
+    }
+}
