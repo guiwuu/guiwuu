340253efc15f06e60be9c9445a4bc25818b351d7
guiwuu <guiwuu@gmail.com>
2011-09-19 03:11:21 +0800

    resolve exprience extension for chrome can not replace links in ajax problem

--- chrome/experience/content.js
+++ chrome/experience/content.js
@@ -1,8 +1,13 @@
-var links = document.all.tags("a");
-for(var i in links){
-	var href = links[i].href;
-	if(href != undefined && href.match('order/creator_history_new.htm') != null){
-		href = href.replace(/&.+$/, '').replace('order/creator_history_new.htm','experience/wall.htm');
-		links[i].href = href;
+function replaceCustomFollowLink(){
+	console.log('replace');
+	var links = document.all.tags("a");
+	for(var i in links){
+		var href = links[i].href;
+		if(href != undefined && href.match('order/creator_history_new.htm') != null){
+			href = href.replace(/&.+$/, '').replace('order/creator_history_new.htm','experience/wall.htm');
+			links[i].href = href;
+		}
 	}
-}
+};
+
+window.setInterval(replaceCustomFollowLink, 100);
