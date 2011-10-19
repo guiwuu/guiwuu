a6c610f3b684258df5b22ee9960c0922af4b94be
guiwuu <guiwuu@gmail.com>
2011-10-19 21:49:41 +0800

    restore experience 0.2 version

--- chrome/experience/content.js
+++ chrome/experience/content.js
@@ -9,4 +9,23 @@ function replaceCustomFollowLink(){
 	}
 };
 
+function addMineExperienceLink(){
+	var mineExperienceLink = location.href.replace(/\.com\/.*/,'.com') + "/lottery/experience/mine.htm";
+	var mineExperienceAnchor = "<a target='_blank' href='" + mineExperienceLink + "'>\u6211\u7684\u6218\u7EE9</a>";
+
+	var quickLink = document.all.item("quick-link");
+	if (quickLink != undefined) {
+		var rel = quickLink.rel;
+		rel = rel.replace('</dl>', '<dd>') + mineExperienceAnchor + '</dd></dl>';
+		quickLink.rel = rel;
+	}
+
+	var myLotteryMenu = document.all.item("mylottery-menu");
+	console.debug(myLotteryMenu);
+	if (myLotteryMenu != undefined) {
+		myLotteryMenu.innerHTML = myLotteryMenu.innerHTML + "<li>" + mineExperienceAnchor + "</li>";
+	}
+}
+
+addMineExperienceLink();
 window.setInterval(replaceCustomFollowLink, 100);
