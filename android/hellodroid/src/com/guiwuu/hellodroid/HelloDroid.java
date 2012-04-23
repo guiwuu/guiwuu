1f112c4edbf8b906c6e4e3026111b8530da3f3eb
guiwuu <guiwuu@gmail.com>
2012-04-23 22:43:30 +0800

    add hellodroid project

---
+++ android/hellodroid/src/com/guiwuu/hellodroid/HelloDroid.java
@@ -0,0 +1,15 @@
+package com.guiwuu.hellodroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelloDroid extends Activity
+{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
