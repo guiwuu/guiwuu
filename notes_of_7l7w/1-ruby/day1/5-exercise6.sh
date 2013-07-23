4a5f74b6aada073c041dda31af51df92e20ea7c1
guiwuu <guiwuu@gmail.com>
2013-07-23 23:54:09 +0800

    add execute result

---
+++ notes_of_7l7w/1-ruby/day1/5-exercise6.sh
@@ -0,0 +1,16 @@
+$ ruby 5-exercise6.rb 
+Guess what I thought, please input a number between 0~9:
+1
+Oops, it is smaller than mine, please input again:
+2
+Oops, it is smaller than mine, please input again:
+3
+Oops, it is smaller than mine, please input again:
+4
+Oops, it is smaller than mine, please input again:
+5
+Oops, it is smaller than mine, please input again:
+6
+Oops, it is smaller than mine, please input again:
+7
+U got me!
