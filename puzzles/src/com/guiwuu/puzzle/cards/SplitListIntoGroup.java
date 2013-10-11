bebba794a887bca0352944482cea7d9918025128
guiwuu <guiwuu@gmail.com>
2013-10-11 10:12:59 +0800

    update SplitListIntoGroup quiz

--- puzzles/src/com/guiwuu/puzzle/cards/SplitListIntoGroup.java
+++ puzzles/src/com/guiwuu/puzzle/cards/SplitListIntoGroup.java
@@ -1,6 +1,8 @@
 package com.guiwuu.puzzle.cards;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Give you a list of uppercase letters (from 'A' to 'Z'), you have to split them into groups based on below rules:
@@ -40,6 +42,7 @@ public class SplitListIntoGroup {
         test("AAABBBCCC", 6);
         test("AAABCDEFUXYZ", 6);
         test("AAABBBCCCDDDEEEFFFGGGHHH", 3);
+        test("ABCDEEFFGHIJK", 2);
     }
 
     static void test(String input, int expect) {
@@ -47,6 +50,8 @@ public class SplitListIntoGroup {
             throw new AssertionError();
         }
     }
+
+    private static final char TOKEN_SPLIT = ',';
 
     private static final char TOKEN_BEGIN = 'b';
 
@@ -61,97 +66,57 @@ public class SplitListIntoGroup {
     private static final int CONSECUTIVE_LIMIT = 5;
 
     static int SplitLetterList(String s) {
-        String prepared = prepare(s);
-        return split(prepared);
+        int count = 0;
+        for (String sub : prepare(s)) {
+            SplitResult result = split(sub);
+            count+=result.sum();
+        }
+        return count;
     }
 
-    private static int split(String prepared) {
-        int len = prepared.length();
-        if (len == 0) {
-            return 0;
-        } else if (len == 1) {// meets rule 1
-            return 1;
-        }
-
-        // check first two characters whether meets any rule or not
-        char second = prepared.charAt(1);
-
-        // meets no rule, which means the first itself makes a group, and recursively split the rest
-        if (second == TOKEN_JUMP) {
-            return 1 + split(prepared.substring(1));
-        }
-
-        // only two possible cases exist, equal or consecutive
-        int equalSplits = Integer.MAX_VALUE;
-        int consecutiveSplits = Integer.MAX_VALUE;
-
-        // meets rule2, try making the first two a group and recursively split the rest
-        if (second == TOKEN_EQUAL) {
-            equalSplits = 1 + split(prepared.substring(2));
-        }
-
-        // meets rule 2 only, return the result
-        if (equalSplits < Integer.MAX_VALUE && len < CONSECUTIVE_LIMIT) {
-            return equalSplits;
-        }
-
-        StringBuilder sb = new StringBuilder();
-        int consecutiveCount = 1;
-        boolean isBreak = false;// ending flag of finding consecutive characters
-        for (int i = 1; i < len; i++) {
-            char c = prepared.charAt(i);
-            if (isBreak) {
-                sb.append(c);
-            } else if (c == TOKEN_JUMP) {// end of finding
-                sb.append(c);
-                isBreak = true;
-            } else if (c == TOKEN_EQUAL) {// equal character, append the former
-                sb.append(prepared.charAt(i - 1));
-            } else if (c == TOKEN_CONSECUTIVE) {// finds a consecutive character
-                consecutiveCount++;
-            }
-        }
-
-        // meets rule3, try make these consecutive characters a group and recursively split the rest
-        if (consecutiveCount >= CONSECUTIVE_LIMIT) {
-            consecutiveSplits = 1 + split(sb.toString());
-        }
-
-        // meets no rule, which means the first itself makes a group, and recursively split the rest
-        if (equalSplits == Integer.MAX_VALUE && consecutiveSplits == Integer.MAX_VALUE) {
-            return 1 + split(prepared.substring(1));
-        }
-        return Math.min(equalSplits, consecutiveSplits);// return smaller number of two split solutions
+    private static SplitResult split(String sub) {
+        SplitResult result = new SplitResult();
+        SplitRuleResult ruleResult = new SplitRuleResult();
+        return result;
     }
 
-    private static String prepare(String input) {
+    static class SplitResult {
+        private int rule1 = 0;
+        private int rule2 = 0;
+        private int rule3 = 0;
+
+        public int sum() {
+            return rule1 + rule2 + rule3;
+        }
+    }
+
+    static class SplitRuleResult {
+        private int splitCount;
+        private String rest;
+    }
+
+    private static String[] prepare(String input) {
         if (input == null) {
             throw new NullPointerException("Input is null!");
         }
 
         char[] chars = input.toCharArray();
         Arrays.sort(chars);
+
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < chars.length; i++) {
             char c = chars[i];
             if (c < 'A' || c > 'Z') {
                 throw new IllegalArgumentException("Found a non-uppercase letter at index: " + i);
             }
-            if (i == 0) {
-                sb.append(TOKEN_BEGIN);
-            } else {
-                int diff = c - chars[i - 1];
-                if (diff < 0) {
-                    sb.append(TOKEN_NEGATIVE);
-                } else if (diff == 0) {
-                    sb.append(TOKEN_EQUAL);
-                } else if (diff == 1) {
-                    sb.append(TOKEN_CONSECUTIVE);
-                } else {
-                    sb.append(TOKEN_JUMP);
-                }
+            sb.append(c);
+            if (i > 0) {
+               int diff = chars[i+1] - c;
+               if (diff > 1) {
+                   sb.append(TOKEN_SPLIT);
+               }
             }
         }
-        return sb.toString();
+        return sb.toString().split(TOKEN_SPLIT+"");
     }
 }
