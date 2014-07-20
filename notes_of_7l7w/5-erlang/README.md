960b45baf4875d1d359592c2518eadd9daeaf5e8
guiwuu <guiwuu@gmail.com>
2014-07-21 02:43:40 +0800

    add to erlang day3

--- notes_of_7l7w/5-erlang/README.md
+++ notes_of_7l7w/5-erlang/README.md
@@ -5,5 +5,7 @@ Erlang User's Guide http://www.erlang.org/doc/getting_started/intro.html
 Erlang STDLIB Reference Manual http://erlang.org/doc/man/STDLIB_app.html
 OTP Design User's Guide http://www.erlang.org/doc/design_principles/users_guide.html
 OTP Reference Page Index http://www.erlang.org/doc/man_index.html
+OTP Design Principles Overview http://www.erlang.org/doc/design_principles/des_princ.html
 Supervisor Behaviour http://www.erlang.org/doc/design_principles/sup_princ.html
+Supervisors in Erlang http://www.hccp.org/supervisors.html
 Gen_Server Behaviour http://www.erlang.org/doc/design_principles/gen_server_concepts.html
---
+++ notes_of_7l7w/5-erlang/day3/5.5-exercise5/translate_service.erl
@@ -0,0 +1,54 @@
+-module(translate_service).
+-behaviour(gen_server).
+-export([start/2]).
+-export([recv/1]).
+-export([connect/1]).
+-export([init/1, handle_call/3, handle_cast/2, code_change/3, handle_info/2, terminate/2]).
+
+-define(TCP_OPTS, [binary, {packet, raw}, {nodelay, true}, {reuseaddr, true}, {active, once}]).
+
+start(Service, Port) ->
+    gen_server:start_link({local, Service}, ?MODULE, Port, []).
+
+init(Port) ->
+    case gen_tcp:listen(Port, ?TCP_OPTS) of
+        {ok, Listen} -> 
+        	spawn(?MODULE, connect, [Listen]),
+    		io:format("~p Server Started.~n", [erlang:localtime()]);
+        Error ->
+            io:format("Error: ~p~n", [Error])
+    end,
+    {ok, self()}.
+
+connect(Listen) ->
+    {ok, Socket} = gen_tcp:accept(Listen),
+    inet:setopts(Socket, ?TCP_OPTS),
+    spawn(fun() -> connect(Listen) end),
+    recv(Socket),
+    gen_tcp:close(Socket).
+
+recv(Socket) ->
+    inet:setopts(Socket, [{active, once}]),
+    receive
+        {tcp, Socket, Data} ->
+            io:format("~p ~p ~p~n", [inet:peername(Socket), erlang:localtime(), Data]),
+            gen_tcp:send(Socket, Data),
+            recv(Socket);
+        {tcp_closed, Socket} ->
+            io:format("~p Client Disconnected.~n", [erlang:localtime()])
+    end.
+
+handle_call(_, _, _) ->
+	undef.
+
+handle_cast(_, _) ->
+	undef.
+
+code_change(_, _, _) ->
+	undef.
+
+handle_info(_, _) ->
+	undef.
+
+terminate(_, _) ->
+	undef.
